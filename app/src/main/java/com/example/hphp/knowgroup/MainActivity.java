bc43d8c2f4844143f0f9504f7674cf15aee60362
moisesrqd@gmail.com <moises123q>
2018-10-21 20:04:23 -0300

    diseño

--- app/src/main/java/com/example/hphp/knowgroup/MainActivity.java
+++ app/src/main/java/com/example/hphp/knowgroup/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.hphp.knowgroup;
 
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.button.MaterialButton;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import android.view.LayoutInflater;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
